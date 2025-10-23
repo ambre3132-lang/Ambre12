18a8fd6fbd4b678033216fbccee044bdcb093e1f
zaidurada1 <zaidurada1@gmail.com>
2025-10-23 16:38:00 +0100

    feat: Simplify Firebase initialization
    Remove redundant check for existing Firebase app. Initialize the app directly to simplify the configuration.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,7 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
