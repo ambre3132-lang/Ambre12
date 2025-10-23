37c82dd02df7b31a0621b75741f2331e49705d94
zaidurada1 <zaidurada1@gmail.com>
2025-10-23 16:50:01 +0100

    feat: Update Firebase configuration
    Reflects recent changes to the Firebase project setup for Ambre.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,25 +1,16 @@
-// Import the functions you need from the SDKs you need
+// Import Firebase
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBrDUVQmmrFt9ruKu3jkC3dK4j2dX2UStU",
-  authDomain: "ambre1.firebaseapp.com",
-  projectId: "ambre1",
-  storageBucket: "ambre1.firebasestorage.app",
-  messagingSenderId: "493636474921",
-  appId: "1:493636474921:web:86e3349154abfee11ae438",
-  measurementId: "G-YJZR99BH8P"
+  apiKey: "AIzaSyAsOdFdD9Tj3_7oFC3poyfO-8sOk5d6COE",
+  authDomain: "ambre-ec987.firebaseapp.com",
+  projectId: "ambre-ec987",
+  storageBucket: "ambre-ec987.firebasestorage.app",
+  messagingSenderId: "951272911775",
+  appId: "1:951272911775:web:269a9e74631da3e19f03aa",
+  measurementId: "G-YG9RQH47YB"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
